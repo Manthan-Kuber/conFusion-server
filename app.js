32caf3956abfc7a741252b6c48db90d9e1eddac8
Manthan Kuber <manthankuber873@gmail.com>
2022-03-02 07:06:48 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -1,61 +1,94 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
 //Morgan responsible for logs on the cmd
-var logger = require('morgan');
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter')
-var promoRouter = require('./routes/promoRouter')
-var leaderRouter = require('./routes/leaderRouter')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes')
+const Dishes = require("./models/dishes");
 
-const url = 'mongodb://localhost:27017/conFusion'
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
 //Establish connection to the server
-connect.then((db) => {
-  console.log('Connected Correctly to server');
-} ,(err) => console.log(err) );
-
+connect.then(
+  (db) => {
+    console.log("Connected Correctly to server");
+  },
+  (err) => console.log(err)
+);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+//The app will use the middleware in the below sequence. thats why before serving up static files from our server we do authentication first
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes',dishRouter)
-app.use('/promotions',promoRouter)
-app.use('/leaders',leaderRouter)
+function auth(req,res,next) {
+  console.log(req.headers)
+  var authHeader = req.headers.authorization;
+  if(!authHeader){
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate','Basic')
+    err.status = 401;
+    return next(err);
+  }
+  //1st element of the split array will be basic and 2nd will be base64 encoding
+  //2nd Split array will split username and password
+  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+  var username = auth[0];
+  var password = auth[1];
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+  if(username === 'admin' && password ==='password'){
+    //Client is authenticated so pass the request tot the next middleware
+    next();
+  }
+  else{
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate','Basic')
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
+// Catch 404 and forward to error handler
+// Next passes the request from one middleware to the next middleware
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
