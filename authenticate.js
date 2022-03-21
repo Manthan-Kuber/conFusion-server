3247c48a3bd8813654777c9001d6e3c96d06af1a
Manthan Kuber <manthankuber873@gmail.com>
2022-03-21 07:28:42 +0530

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require("./models/user");
 var JwtStrategy = require("passport-jwt").Strategy;
 var ExtractJwt = require("passport-jwt").ExtractJwt;
 var jwt = require("jsonwebtoken");
+var FacebookTokenStrategy = require("passport-facebook-token");
 
 var config = require("./config");
 
@@ -36,7 +37,7 @@ exports.jwtPassport = passport.use(
 
 exports.verifyUser = passport.authenticate("jwt", { session: false }); //We are not creating sessions as we are using session based authentication //.authenticate(strategy,options)
 
-exports.verifyAdmin = (req,res,next) => {
+exports.verifyAdmin = (req, res, next) => {
   if (req.user.admin) next();
   else {
     let err = new Error("You are not authorized to perform this operation!");
@@ -45,4 +46,27 @@ exports.verifyAdmin = (req,res,next) => {
   }
 };
 
-
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) return done(err, false);
+        if (!err && user !== null) return done(null, user);
+        else {
+          user = new User({ username: profile.displayName });
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) return done(err, false);
+            else return done(null, user);
+          });
+        }
+      });
+    }
+  )
+);
--- config.js
+++ config.js
@@ -2,5 +2,9 @@
 
 module.exports = {
     "secretKey" : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    "facebook" : {
+        clientId: "981200326123820",
+        clientSecret: "849eb41a1c1d1a7aaf11fb37191b7116",
+    }
 }
